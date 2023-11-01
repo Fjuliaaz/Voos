1077a9a885705efdd21020916a7a7623051531c6
JULIA APARECIDA DA SILVA FONSECA <23111808@CFP127ALUNO.LOCAL>
2023-11-01 13:36:49 -0300

    first commit

---
+++ src/br/senai/sp/jandira/model/Cadastro.java
@@ -0,0 +1,30 @@
+package br.senai.sp.jandira.model;
+
+public class Cadastro {
+        private String companhias, origem, destino, horaSaida, dataPartida;
+        private int Npassageiros, numeroVoo;
+
+        public Cadastro(String companhias, String destino, String origem, String horaSaida,
+                            String dataPartida, Integer numeroVoo, Integer Npassageiros){
+
+            this.companhias = companhias;
+            this.destino = destino;
+            this.origem = origem;
+            this.horaSaida = horaSaida;
+            this.dataPartida = dataPartida;
+            this.numeroVoo = numeroVoo;
+            this.Npassageiros = Npassageiros;
+        }
+
+        public String print() {
+            return "Companhias: " + companhias + "\n" +
+                    "Destino: " + destino + "\n" +
+                    "Origem: " + origem + "\n" +
+                    "Hora de Saída: " + horaSaida + "\n" +
+                    "Data de Saída: " + dataPartida + "\n" +
+                    "Número do Voo: " + numeroVoo + "\n" +
+                    "Numero de Passageiros: " + Npassageiros;
+        }
+    }
+
+
